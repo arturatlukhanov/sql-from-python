dfea1f7a294dd5c67eeefea65544d4549333a0f3
arturatlukhanov <154214872+arturatlukhanov@users.noreply.github.com>
2025-02-19 15:07:40 +0300

    Update sql.py

--- sql.py
+++ sql.py
@@ -3,32 +3,26 @@ import pandas as pd
 import datetime
 from datetime import timedelta
 from io import StringIO
-#import win32com.client
 from imap_tools import MailBox
 from glob import glob
 import os
-import aiohttp
-import asyncio
-import numpy as np
 from mytracker_export_api import MyTracker
 from sshtunnel import SSHTunnelForwarder
-from zipfile import ZipFile
 from sqlalchemy import create_engine
 import psycopg2
-from termcolor import colored
 import requests
 
 with SSHTunnelForwarder(    
-    ('49.12.77.59', 22), #Remote server IP and SSH port
-    ssh_username = "analitytic",
-    ssh_password = "BAXqwCZukkdPW6OFbUnW",
-    remote_bind_address=('127.0.0.1', 5432)
+    ('', ), #Remote server IP and SSH port
+    ssh_username = "",
+    ssh_password = "",
+    remote_bind_address=(')
     ) as server:
 
     server.start()
 
     local_port = str(server.local_bind_port)
-    engine = create_engine('postgresql://analist:xl3KQugjnp@127.0.0.1:' + local_port +'/ad_stats_system_db') 
+    engine = create_engine('postgresql://' + local_port +'/') 
 
     octa_daily = pd.read_sql(
         f'''
